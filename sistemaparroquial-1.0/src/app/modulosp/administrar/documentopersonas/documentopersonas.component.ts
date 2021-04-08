90c58d03f741b88199fc02a7a4574a2ca6995a1f
manolo-ca <manolocordova999@gmail.com>
2021-04-08 10:10:38 -0500

    angula

--- sistemaparroquial-1.0/src/app/modulosp/administrar/documentopersonas/documentopersonas.component.ts
+++ sistemaparroquial-1.0/src/app/modulosp/administrar/documentopersonas/documentopersonas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Documentopersonasejemplo } from './documentopersonasejemplo';
+
 @Component({
   selector: 'app-documentopersonas',
   templateUrl: './documentopersonas.component.html'
