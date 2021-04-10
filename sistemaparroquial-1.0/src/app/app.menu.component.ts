35d5179693cdaf5ff505090ac98c1d0e100c185d
manolo-ca <manolocordova999@gmail.com>
2021-04-10 18:38:30 -0500

    actualizacion6pm100421

--- sistemaparroquial-1.0/src/app/app.menu.component.ts
+++ sistemaparroquial-1.0/src/app/app.menu.component.ts
@@ -50,7 +50,7 @@ export class AppMenuComponent implements OnInit {
                         {label: 'Administrar Solicitudes de documentos', icon: 'fa fa-fw fa-minus-square-o',routerLink:'modulosp/administrar/emiciondocumentos'},
                        
                         {label: 'Administrar tipo de eucaristias', icon: 'fa fa-fw fa-minus-square-o',routerLink:'modulosp/administrar/tipoeucaristias'},
-                        {label: 'Administrar Inscricion a eucaristias', icon: 'fa fa-fw fa-minus-square-o',routerLink:'modulosp/administrar/inscripcioneucaristias'},
+                        {label: 'Administrar Inscripcion a eucaristias', icon: 'fa fa-fw fa-minus-square-o',routerLink:'modulosp/administrar/inscripcioneucaristias'},
                         {label: 'Administrar Reservacion eucaristias', icon: 'fa fa-fw fa-minus-square-o',routerLink:'modulosp/administrar/reservacioneucaristias'}
                     ]
                     },
